3a1177217052a8421f8114a2694c775604b14f6b
Jeremy Stucki <jeremy@interactivethings.com>
2016-01-05 10:39:36 +0100

    Add code and readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # responsive-loader
-A webpack loader for responsive images
+
+A webpack loader for responsive images. Creates multiple images from one source image, and returns a `srcset`. For more information on how to use `srcset`, read [Responsive Images: If you’re just changing resolutions, use srcset.](https://css-tricks.com/responsive-images-youre-just-changing-resolutions-use-srcset/). Browser support is [pretty good](http://caniuse.com/#search=srcset).
+
+## Install
+
+```
+npm install responsive-loader jimp --save-dev
+```
+
+responsive-loader uses [jimp](https://github.com/oliver-moran/jimp), a pure JS image manipulation library (so no other dependencies, yay :v:), to transform images which needs to be installed alongside responsive-loader.
+
+## Usage
+
+```js
+// Outputs three images with 100, 200, and 300px widths
+const responsiveImage = require('responsive?sizes[]=100,sizes[]=200,sizes[]=300!myImage.jpg');
+
+// responsiveImage.srcSet => '2fefae46cb857bc750fa5e5eed4a0cde-100.jpg 100w,2fefae46cb857bc750fa5e5eed4a0cde-200.jpg 200w,2fefae46cb857bc750fa5e5eed4a0cde-300.jpg 300w'
+React.render(<div srcSet={responsiveImage.srcSet} />, el);
+```
+
+### Options
+
+- `sizes: array`: specify all widths you want to use; if a specified size exceeds the original image's width, the latter will be used (i.e. images won't be scaled up)
+- `quality: integer`: JPEG compression quality; defaults to `95`
+- `ext: string`: either `png`, `jpg`, or `gif`; use to convert to another format; defaults to original file's extension
+- `background: hex`: Background fill when converting transparent to opaque images; defaults to `0xFFFFFFFF` (note: make sure this is a valid hex number)
+
+## Notes
+
+- Doesn't support `1x`, `2x` sizes.
+- The image hash is based on the original image's content plus a size suffix, e.g. `2fefae46cb857bc750fa5e5eed4a0cde-100.jpg`, `2fefae46cb857bc750fa5e5eed4a0cde-200.jpg`, etc.
+
+## See also
+
+- Inspired by [resize-image-loader](https://github.com/Levelmoney/resize-image-loader), but simpler and without dependency on ImageMagick
+
---
+++ index.js
@@ -0,0 +1,83 @@
+/* eslint-disable */
+var path = require('path');
+var loaderUtils = require('loader-utils');
+var jimp = require('jimp');
+var queue = require('queue-async');
+
+var MIMES = {
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'png': 'image/png',
+  'gif': 'image/gif'
+};
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+  var loaderCallback = this.async();
+  var query = loaderUtils.parseQuery(this.query);
+  var sizes = query.sizes;
+  var name = query.name || '[hash]-[width].';
+  // JPEG compression
+  var quality = parseInt(query.quality, 10) || 95;
+  // Useful when converting from PNG to JPG
+  var background = parseInt(query.background, 16) || 0xFFFFFFFF;
+  // Specify ext to convert to another format
+  var ext = query.ext || path.extname(this.resourcePath).replace(/\./, '');
+  var mime = MIMES[ext];
+  var loaderContext = this;
+
+  if (!sizes) {
+    return loaderCallback(null, content);
+  }
+
+  if (!mime) {
+    return loaderCallback(new Error('No mime type for file with extension ' + ext + 'supported'));
+  }
+
+  jimp.read(loaderContext.resourcePath, function(err, img) {
+    if (err) {
+      return queueCallback(err);
+    }
+
+    function resizeImage(width, queueCallback) {
+        img
+          .clone()
+          .resize(width, jimp.AUTO)
+          .quality(quality)
+          .background(background)
+          .getBuffer(mime, function(err, buf) {
+            if (err) {
+              return queueCallback(err);
+            }
+
+            var fileName = loaderUtils.interpolateName(loaderContext, '[hash]-[width].' + ext, {content: content}).replace(/\[width\]/g, width);
+
+            loaderContext.emitFile(fileName, buf);
+
+            queueCallback(null, {
+              src: '__webpack_public_path__ + ' + JSON.stringify(fileName + ' ' + width + 'w'),
+              path: '__webpack_public_path__ + ' + JSON.stringify(fileName),
+              width: width
+            });
+          });
+    }
+
+    var q = queue();
+    
+    sizes.forEach(function(size) {
+      var width = Math.min(img.bitmap.width, parseInt(size, 10));
+
+      q.defer(resizeImage, width);
+    });
+
+    q.awaitAll(function(err, files) {
+      var srcset = files.map(function(f) {
+        return f.src;
+      }).join('+","+');
+      
+      loaderCallback(null, 'module.exports = {srcSet:' + srcset + '};');
+    });
+  });
+};
+
+module.exports.raw = true; // get buffer stream instead of utf8 string
