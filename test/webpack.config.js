e296edfed6adb175cf58ee4e7cc308f43aa41a51
Jeremy Stucki <jeremy@interactivethings.com>
2016-05-17 20:35:52 +0200

    Test for webpack publicPath support

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'index'),
   output: {
     path: path.resolve(__dirname, 'build'),
+    publicPath: 'foobar/',
     filename: 'test.js'
   },
   target: 'node'
