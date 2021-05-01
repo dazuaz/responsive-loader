2ebbf51cbc5d17711cd2ed8dd2697fde7fe24291
Daniel Zuloaga <danielzuloaga@gmail.com>
2021-05-01 09:59:22 -0400

    preparing for examples

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["examples/*"],
   rules: {
     "@typescript-eslint/ban-ts-comment": "off",
   },
